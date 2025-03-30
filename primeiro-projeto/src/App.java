71a3fcd3990b31134aa37113e37b0419d52fcec3
Murilo Santana <ssz.mu04@gmail.com>
2025-03-29 21:14:40 -0300

    tentativa 500

--- primeiro-projeto/src/App.java
+++ primeiro-projeto/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("MEU DEUSSSSS EU NÃO AGUENTO MAIS JAVAAA É COISA DE MALUUUCOOOOOO AHHHHHHHHHHHHHHHHH!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println("meu deusss cara nada funcionaaa aquiiiiiiiii");
     }
 }
