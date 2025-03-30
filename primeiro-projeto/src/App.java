33693d5fac0e4bf1f140c4a3de4969d696c52d5c
Murilo Santana <ssz.mu04@gmail.com>
2025-03-29 21:13:24 -0300

    primeiro arquivo java

---
+++ primeiro-projeto/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("MEU DEUSSSSS EU NÃO AGUENTO MAIS JAVAAA É COISA DE MALUUUCOOOOOO AHHHHHHHHHHHHHHHHH!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+}
