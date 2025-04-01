6c207c0d162c0d6891ada63b61880dee3dd97078
Murilo Santana <ssz.mu04@gmail.com>
2025-04-01 08:24:57 -0300

    declaração de variável

---
+++ anatomia-classes/src/Soma.java
@@ -0,0 +1,15 @@
+public class Soma {
+  
+  public static void main(String[] args) {
+      
+    int numero1, numero2, resultado;
+
+    numero1 = 3;
+    numero2 = 4;
+
+    resultado = numero1 + numero2;
+
+    System.out.println(resultado);
+  }
+}
+ 
