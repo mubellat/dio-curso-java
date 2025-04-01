2a5c3a280c2384306bc0399a1d4332f804a920f0
Murilo Santana <ssz.mu04@gmail.com>
2025-04-01 10:38:11 -0300

    condicionais

---
+++ variavel-java/Condicao.java
@@ -0,0 +1,13 @@
+public class Condicao {
+  
+  public static void main(String[] args) {
+      
+    int a, b;
+    a = 3;
+    b = 3;
+
+    String resultado = a==b ? "verdadeiro" : "falso";
+
+    System.out.println(resultado);
+  }
+}
---
+++ variavel-java/Incremento.java
@@ -0,0 +1,15 @@
+public class Incremento {
+  
+  public static void main(String[] args) {
+      
+    int numero =5;
+
+    numero++;
+
+    System.out.println(--numero);
+
+    boolean variavel = true;
+
+    System.out.println(!variavel);
+  }
+}
