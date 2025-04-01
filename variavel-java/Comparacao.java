6a17bffe912388afde5628a2b6f12dc6a2c454fe
Murilo Santana <ssz.mu04@gmail.com>
2025-04-01 12:00:45 -0300

    validacao

---
+++ variavel-java/Comparacao.java
@@ -0,0 +1,16 @@
+public class Comparacao {
+  
+  public static void main(String[] args) {
+    
+    int numero1 = 1, numero2 = 2;
+
+    boolean simNao = numero1 == numero2;
+
+    System.out.println("numeroUm é igual a numeroDois?" + simNao);
+
+    simNao = numero1 != numero2;
+
+    System.out.println("numeroUm é diferente ao numeroDois?");
+    System.out.println(simNao);
+  }
+}
