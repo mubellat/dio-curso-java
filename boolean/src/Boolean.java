b4dd6f471b7d91d1dedfd70189640583fbf55f7e
Murilo Santana <ssz.mu04@gmail.com>
2025-05-05 02:37:48 -0300

    logica booleana

---
+++ boolean/src/Boolean.java
@@ -0,0 +1,26 @@
+public class Boolean {
+    public static void main(String[] args) {
+        int i1 = 10;
+        int i2 = 20;
+
+        float f1 = 4.5f;
+        float f2 = 3.5f;
+
+        boolean b1 = true;
+        boolean b2 = false;
+        boolean b3 = true;
+        boolean b4 = false;
+
+        System.out.println("b1 && b2"+ (b1 && b2));
+        System.out.println("b1 && b3"+ (b1 && b3));
+
+        System.out.println("b2 || b3"+ (b2 || b3));
+        System.out.println("b2 || b4"+ (b2 || b4));
+
+        System.out.println("b1 ^ b3"+ (b1 ^ b3));
+        System.out.println("b4 ^ b1"+ (b4 ^ b1));
+
+        System.out.println("(i1>i2) || (f2<f1) "+ ((i1>i2) || (f2<f1)));
+        System.out.println("((i1 + i2)<(f2 - f1)) && true "+ (((i1 + i2)<(f2 - f1)) && true));
+    }
+}
